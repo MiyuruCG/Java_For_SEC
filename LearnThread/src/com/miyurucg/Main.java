ecca032c1aa3de4714ffd042e2eff391df1b8764
MiyuruCG <miyurugunatilake@gmail.com>
2020-09-14 09:38:55 +0530

    Using Lambda expressions

--- LearnThread/src/com/miyurucg/Main.java
+++ LearnThread/src/com/miyurucg/Main.java
@@ -1,40 +1,25 @@
 package com.miyurucg;
 
-import java.util.EmptyStackException;
-
-class Hi implements Runnable
-{
-    public void run(){
-        for(int i = 1;i<=5;i++){
-            System.out.println("Hi");
-            try{Thread.sleep(1000);} catch(Exception e){}
-        }
-    }
-}
-
-class Hello implements Runnable
-{
-    public void run(){
-        for(int i = 1;i<=5;i++){
-            System.out.println("Hello");
-            try{Thread.sleep(1000);} catch(Exception e){}
-
-        }
-    }
-}
-
 public class Main {
 
     public static void main(String[] args) {
 
-        Hi obj1 = new Hi();
-        Hello obj2 = new Hello();
 
-        Thread t1 = new Thread(obj1);
-        Thread t2 = new Thread(obj2);
+        Thread t1 = new Thread(()-> {
+            for(int i = 1;i<=5;i++){
+                System.out.println("Hi");
+                try{Thread.sleep(1000);} catch(Exception e){System.out.println(e.getMessage());}
+            }
+        });
+        Thread t2 = new Thread(()-> {
+            for(int i = 1;i<=5;i++){
+                System.out.println("Hello");
+                try{Thread.sleep(1000);} catch(Exception e){System.out.println(e.getMessage());}
+            }
+        });
 
         t1.start();
-        try{Thread.sleep(10);} catch(Exception e){}
+        try{Thread.sleep(10);} catch(Exception e){System.out.println(e.getMessage());}
         t2.start();
 
     }
