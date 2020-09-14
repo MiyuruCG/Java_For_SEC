28739a6595706ece39c47d52b27a60744794ff9e
MiyuruCG <miyurugunatilake@gmail.com>
2020-09-14 09:50:19 +0530

    Thread Pritrity

--- LearnThread/src/com/miyurucg/Main.java
+++ LearnThread/src/com/miyurucg/Main.java
@@ -9,19 +9,26 @@ public class Main
 
         Thread t1 = new Thread(()-> {
             for(int i = 1;i<=5;i++){
-                System.out.println("Hi");
+                System.out.println("Hi "+Thread.currentThread().getPriority());
                 try{Thread.sleep(1000);} catch(Exception e){System.out.println(e.getMessage());}
             }
-        });
+        }, "Hi-Thread");
         Thread t2 = new Thread(()-> {
             for(int i = 1;i<=5;i++){
-                System.out.println("Hello");
+                System.out.println("Hello "+Thread.currentThread().getPriority());
                 try{Thread.sleep(1000);} catch(Exception e){System.out.println(e.getMessage());}
             }
-        });
+        }, "Hello-Thread");
+
+//        System.out.println(t1.getName());
+//        System.out.println(t2.getName());
+        t2.setPriority(Thread.MIN_PRIORITY);
+        t1.setPriority(Thread.MAX_PRIORITY);
+//        System.out.println(t1.getPriority());
+//        System.out.println(t2.getPriority());
 
         t1.start();
-        try{Thread.sleep(10);} catch(Exception e){System.out.println(e.getMessage());}
+        //try{Thread.sleep(10);} catch(Exception e){System.out.println(e.getMessage());}
         t2.start();
 
         t1.join();
